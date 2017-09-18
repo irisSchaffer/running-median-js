2d0d86e818a30932891de30acc637bcb5fa35c67
Iris Schaffer <irisschaffer@gmx.net>
2017-09-18 12:28:14 +0200

    Fixes bug in running median + indentation
    - Fixes problem where we defaulted to adding to the heap with fewer
      elements
    - Uses tabs instead of spaces now

--- runningMedian.js
+++ runningMedian.js
@@ -2,45 +2,40 @@ const heap = require('./heap');
 
 const runningMedian = _ => {
 	const minHeap = heap((a, b) => a - b);
-    const maxHeap = heap((a, b) => b - a);
+	const maxHeap = heap((a, b) => b - a);
 
 	const getMedian = _ => {
 		if (minHeap.length() === maxHeap.length()) {
-	        return (minHeap.root() + maxHeap.root()) / 2;
-	    } else {
-	        return minHeap.length() > maxHeap.length()
-                ? minHeap.root()
-                : maxHeap.root();
-	    }
+			return (minHeap.root() + maxHeap.root()) / 2;
+		}
+		return minHeap.length() > maxHeap.length()
+			? minHeap.root()
+			: maxHeap.root();
 	};
 
-    const shouldAddToMaxHeap = val => val < maxHeap.root();
-    const shouldAddToMinHeap = val => val > minHeap.root();
+	const hasToAddToMaxHeap = val => val < maxHeap.root();
+	const hasToAddToMinHeap = val => val > minHeap.root();
 
 	const addValue = val => {
-		if (minHeap.length() === maxHeap.length()) {
-            if (shouldAddToMaxHeap(val)) {
-                maxHeap.insert(val);
-            } else {
-                minHeap.insert(val);
-            }
-        } else if (maxHeap.length() > minHeap.length()) {
-            if (shouldAddToMinHeap()) {
-                const oldRoot = maxHeap.remove();
-                minHeap.insert(oldRoot);
-                maxHeap.insert(val);
-            } else {
-                minHeap.insert(val);
-            }
-        } else {
-            if (shouldAddToMinHeap(val)) {
-                const oldRoot = minHeap.remove();
-                maxHeap.insert(oldRoot);
-                minHeap.insert(val);
-            } else {
-                maxHeap.insert(val);
-            }
-        }
+		if (hasToAddToMinHeap(val)) {
+			if (minHeap.length() > maxHeap.length()) {
+				const oldRoot = minHeap.remove();
+				maxHeap.insert(oldRoot);
+			}
+			minHeap.insert(val);
+		} else if (hasToAddToMaxHeap(val)) {
+			if (maxHeap.length() > minHeap.length()) {
+				const oldRoot = maxHeap.remove();
+				minHeap.insert(oldRoot);
+			}
+			maxHeap.insert(val);
+		} else { // can add to either
+			if (minHeap.length() <= maxHeap.length()) {
+				minHeap.insert(val);
+			} else {
+				maxHeap.insert(val);
+			}
+		}
 	};
 
 	return {
