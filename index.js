2c22931f9e588f61ada74991ee7211feae533cb7
Iris Schaffer <irisschaffer@gmx.net>
2017-09-18 12:30:00 +0200

    Changes wording + fixes indentation

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const readline = require('readline');
 const runningMedian = require('./runningMedian');
 
-console.log('Enter a number and press Enter to add a number to the running median calculator.');
+console.log('To add a number to the running median calculator, enter an integer and press Enter.');
 console.log('You can exit with Ctrl + c');
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+	input  : process.stdin,
+	output : process.stdout
 });
 
 const calculator = runningMedian();
@@ -14,14 +14,14 @@ const calculator = runningMedian();
 rl.on('line', input => {
 	const val = parseInt(input, 10);
 	if (isNaN(val)) {
-		console.log('Not a number!');
+		console.log('Not a number – Try again!');
 		return;
 	}
 
 	console.log(`Adding ${val} to compute running median`);
 	calculator.addValue(val);
 	const median = calculator.getMedian();
-    console.log(median.toFixed(1));
+	console.log(`New median is: ${median.toFixed(1)}`);
 });
 
 rl.on('SIGINT', _ => {
